42a44b5cbccaee9a65729ee908d3f5cfd2018d97
HavizBriliant <86892120+HavizBriliant@users.noreply.github.com>
2021-07-17 23:58:13 +0700

    Add files via upload

--- table.c
+++ table.c
@@ -1,5 +1,7 @@
-#include <windows.h>
-#include <GL/glut.h>
+include <windows.h>
+include <GL/glut.h>
+include <GL/glut.h>
+include <stdlib.h>
 import cv2
 import numpy as np
 
@@ -385,3 +387,99 @@ if __name__ == '__main__':
         # Show the image
         cv2.imshow(wndname2, dst)
         k = cv2.waitKey(1)
+		
+		int specularLightstatus = 1;
+int ambientLightStatus = 1;
+void init(void);
+void display(void);
+void keyboard(unsigned char key, int x, int y);
+void reshape(int w, int h);
+int main(int argc, char** argv)
+{
+   glutInit(&argc, argv);
+   glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+   glutInitWindowSize (500, 500);
+   glutInitWindowPosition (100, 100);
+   glutCreateWindow ("Lighting");
+   init();
+   glutDisplayFunc(display);
+   glutReshapeFunc(reshape);
+   glutKeyboardFunc(keyboard);
+   glutMainLoop();
+   return 0;
+}
+/*  Initialize material property, light source, lighting model,
+ *  and depth buffer.
+ */
+void init(void)
+{
+   GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+   GLfloat mat_shininess[] = { 50.0 };
+   GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
+   glClearColor (0.0, 0.0, 0.0, 0.0);
+   glShadeModel (GL_SMOOTH);
+   glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+   glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+   glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+   glEnable(GL_LIGHTING);
+   glEnable(GL_LIGHT0);
+   glEnable(GL_DEPTH_TEST);
+}
+void display(void)
+{
+   glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+   glutSolidSphere (1.0, 20, 16);
+   glFlush ();
+}
+void reshape (int w, int h)
+{
+   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+   glMatrixMode (GL_PROJECTION);
+   glLoadIdentity();
+   if (w <= h)
+      glOrtho (-1.5, 1.5, -1.5*(GLfloat)h/(GLfloat)w,
+         1.5*(GLfloat)h/(GLfloat)w, -10.0, 10.0);
+   else
+      glOrtho (-1.5*(GLfloat)w/(GLfloat)h,
+         1.5*(GLfloat)w/(GLfloat)h, -1.5, 1.5, -10.0, 10.0);
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
+}
+void keyboard(unsigned char key, int x, int y)
+{
+   switch (key) {
+      case 27:
+         exit(0);
+         break;
+      case 'O':
+      case 'o':
+         if(specularLightstatus == 1)
+         {
+            glDisable(GL_LIGHTING);
+            glutPostRedisplay();
+            specularLightstatus = 0;
+         }
+         else
+         {
+             glEnable(GL_LIGHTING);
+             glutPostRedisplay();
+             specularLightstatus = 1;
+         }
+         break;
+      case 'p':
+      case     'P':
+           if(ambientLightStatus == true)
+         {
+            glDisable(GL_LIGHT0);
+            glutPostRedisplay();
+            ambientLightStatus = false;
+         }
+         else
+         {
+             glEnable(GL_LIGHT0);
+             glutPostRedisplay();
+             ambientLightStatus = true;
+         }
+         break;
+   }
+} 
